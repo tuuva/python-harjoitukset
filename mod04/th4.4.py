8f33490505af133fead25cff3813d567d7977916
Tuuva Tourunen <tourutuu@gmail.com>
2026-08-25 13:26:58 +0300

    tehtävä 4

--- mod04/th4.4.py
+++ mod04/th4.4.py
@@ -1,5 +1,6 @@
 vuosiluku = float(input("Anna vuosiluku?"))
-if vuosiluku % 4 == 0 or vuosiluku % 400 == 0:
+if vuosiluku <100 and vuosiluku % 4 == 0 or vuosiluku >100 and vuosiluku % 400 == 0:
     print("On karkausvuosi.")
 else:
     print("Ei ole karkausvuosi")
+
